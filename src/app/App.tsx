d7ca5e6f72d90d42675da8ee987ed0fa5144cfb6
Fiodar <federal8228@gmail.com>
2026-03-24 15:24:16 +0100

    feat: implement fsd architecture and get fonts

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-black flex flex-col gap-2'>
+      <span className='text-white font-jakarta font-bold'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi non facere dicta dolorum sapiente eum nemo inventore? Sequi, eius alias quod porro sint molestias assumenda quia quo blanditiis, nesciunt sed officia minima!</span>
+      <span className='text-gray-500'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi non facere dicta dolorum sapiente eum nemo inventore? Sequi, eius alias quod porro sint molestias assumenda quia quo blanditiis, nesciunt sed officia minima!</span>
+    </div>
+  )
+}
+
+export default App
